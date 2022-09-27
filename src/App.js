ac09fa881f16aa107f8de9095119548fc6f344f8
cbappi <abdulwahedrr@gmail.com>
2022-09-27 20:30:31 +0600

    71+72 Doctor portal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Pages/Shared/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
 import Login from './Pages/Login/Login';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="appointment" element={<Appointment />} />
         <Route path="login" element={<Login />} />
       </Routes>
     </div>
